1a0615cbb555cf0283230c64c811d426ffe3564c
YuudachiXMMY <934647325@qq.com>
2021-04-14 15:51:52 +0800

    remove unzipped past builds

--- m.py
+++ m.py
@@ -153,15 +153,15 @@ def startShadowOfTombRaider():
     except Exception:
         logger.error('Error in Runing ShadowOfTombRaider.main()', exc_info=True)
     else:
-        try:
-            gameHD = lib.screen.findWindow("{GAME_DIRECTORY}".format(GAME_DIRECTORY="Shadow of the Tomb Raider"))
-            if gameHD != 0:
-                try:
-                    statC = utils.killProgress("SOTTR.exe")
-                except Exception:
-                    logger.warning('Killing process Error: ShadowOfTombRaider')
-        except Exception:
-            logger.warning('Error in Finding ShadowOfTombRaider Game Window', exc_info=True)
+        # try:
+        #     gameHD = lib.screen.findWindow("{GAME_DIRECTORY}".format(GAME_DIRECTORY="Shadow of the Tomb Raider"))
+        #     if gameHD != 0:
+        #         try:
+        #             statC = utils.killProgress("SOTTR.exe")
+        #         except Exception:
+        #             logger.warning('Killing process Error: ShadowOfTombRaider')
+        # except Exception:
+        #     logger.warning('Error in Finding ShadowOfTombRaider Game Window', exc_info=True)
         return statusCode
 
 def startSniperEliteV2():
@@ -175,15 +175,15 @@ def startSniperEliteV2():
     except Exception:
         logger.error('Error in Runing SniperEliteV2.main()', exc_info=True)
     else:
-        try:
-            gameHD = lib.screen.findWindow("{GAME_DIRECTORY}".format(GAME_DIRECTORY="SniperEliteV2 Benchmark"))
-            if gameHD != 0:
-                try:
-                    statC = utils.killProgress("SniperEliteV2.exe")
-                except Exception:
-                    logger.warning('Killing process Error: SniperEliteV2')
-        except Exception:
-            logger.warning('Error in Finding SniperEliteV2 Game Window', exc_info=True)
+        # try:
+        #     gameHD = lib.screen.findWindow("{GAME_DIRECTORY}".format(GAME_DIRECTORY="SniperEliteV2 Benchmark"))
+        #     if gameHD != 0:
+        #         try:
+        #             statC = utils.killProgress("SniperEliteV2.exe")
+        #         except Exception:
+        #             logger.warning('Killing process Error: SniperEliteV2')
+        # except Exception:
+        #     logger.warning('Error in Finding SniperEliteV2 Game Window', exc_info=True)
         return statusCode
 
 def startAvP_D3D11():
@@ -197,15 +197,15 @@ def startAvP_D3D11():
     except Exception:
         logger.error('Error in Runing AvP_D3D11.main()', exc_info=True)
     else:
-        try:
-            gameHD = lib.screen.findWindow("{GAME_DIRECTORY}".format(GAME_DIRECTORY="AvP"))
-            if gameHD != 0:
-                try:
-                    statC = utils.killProgress("AvP_D3D11.exe")
-                except Exception:
-                    logger.warning('Killing process Error: AvP_D3D11')
-        except Exception:
-            logger.warning('Error in Finding AvP_D3D11 Game Window', exc_info=True)
+        # try:
+        #     gameHD = lib.screen.findWindow("{GAME_DIRECTORY}".format(GAME_DIRECTORY="AvP"))
+        #     if gameHD != 0:
+        #         try:
+        #             statC = utils.killProgress("AvP_D3D11.exe")
+        #         except Exception:
+        #             logger.warning('Killing process Error: AvP_D3D11')
+        # except Exception:
+        #     logger.warning('Error in Finding AvP_D3D11 Game Window', exc_info=True)
         return statusCode
 
 def startBHScripts():
@@ -232,16 +232,16 @@ def startGenshinImpact():
     except Exception:
         logger.error('Error in Runing GenshinImpact.main()', exc_info=True)
     else:
-        try:
-            gameHD = lib.screen.findWindow("{GAME_DIRECTORY}.exe".format(GAME_DIRECTORY="原神"))
-            if gameHD != 0:
-                try:
-                    statC = utils.killProgress("原神.exe")
-                    # statC = utils.killProgress("launcher.exe")
-                except Exception:
-                    logger.warning('Killing process Error: GenshinImpact')
-        except Exception:
-            logger.warning('Error in Finding GenshinImpact Game Window', exc_info=True)
+        # try:
+        #     gameHD = lib.screen.findWindow("{GAME_DIRECTORY}.exe".format(GAME_DIRECTORY="原神"))
+        #     if gameHD != 0:
+        #         try:
+        #             statC = utils.killProgress("原神.exe")
+        #             # statC = utils.killProgress("launcher.exe")
+        #         except Exception:
+        #             logger.warning('Killing process Error: GenshinImpact')
+        # except Exception:
+        #     logger.warning('Error in Finding GenshinImpact Game Window', exc_info=True)
         return statusCode
 
 def startFallout4():
@@ -255,16 +255,16 @@ def startFallout4():
     except Exception:
         logger.error('Error in Runing Fallout4.main()', exc_info=True)
     else:
-        try:
-            gameHD = lib.screen.findWindow("{GAME_DIRECTORY}.exe".format(GAME_DIRECTORY="Fallout4"))
-            if gameHD != 0:
-                try:
-                    statC = utils.killProgress("Fallout4.exe")
-                    # statC = utils.killProgress("launcher.exe")
-                except Exception:
-                    logger.warning('Killing process Error: Fallout4')
-        except Exception:
-            logger.warning('Error in Finding Fallout4 Game Window', exc_info=True)
+        # try:
+        #     gameHD = lib.screen.findWindow("{GAME_DIRECTORY}.exe".format(GAME_DIRECTORY="Fallout4"))
+        #     if gameHD != 0:
+        #         try:
+        #             statC = utils.killProgress("Fallout4.exe")
+        #             # statC = utils.killProgress("launcher.exe")
+        #         except Exception:
+        #             logger.warning('Killing process Error: Fallout4')
+        # except Exception:
+        #     logger.warning('Error in Finding Fallout4 Game Window', exc_info=True)
         return statusCode
 
 def startOffice():
@@ -291,16 +291,16 @@ def startWeHappyFew():
     except Exception:
         logger.error('Error in Runing WeHappyFew.main()', exc_info=True)
     else:
-        try:
-            gameHD = lib.screen.findWindow("{GAME_DIRECTORY}.exe".format(GAME_DIRECTORY="We Happy Few (64-bit, PCD3D_SM5)"))
-            if gameHD != 0:
-                try:
-                    statC = utils.killProgress("GlimpseGame.exe")
-                    # statC = utils.killProgress("launcher.exe")
-                except Exception:
-                    logger.warning('Killing process Error: WeHappyFew')
-        except Exception:
-            logger.warning('Error in Finding WeHappyFew Game Window', exc_info=True)
+        # try:
+        #     gameHD = lib.screen.findWindow("{GAME_DIRECTORY}.exe".format(GAME_DIRECTORY="We Happy Few (64-bit, PCD3D_SM5)"))
+        #     if gameHD != 0:
+        #         try:
+        #             statC = utils.killProgress("GlimpseGame.exe")
+        #             # statC = utils.killProgress("launcher.exe")
+        #         except Exception:
+        #             logger.warning('Killing process Error: WeHappyFew')
+        # except Exception:
+        #     logger.warning('Error in Finding WeHappyFew Game Window', exc_info=True)
         return statusCode
 
 def main():
